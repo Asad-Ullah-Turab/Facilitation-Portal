a23b99431845a72c2174013399f65b9f776232ac
asad-ullah-turab <asadullahturab47@gmail.com>
2024-05-18 10:54:54 +0500

    add query data submission into mysql database but doesn't update table

---
+++ Frontend/scripts/addquery.js
@@ -0,0 +1,29 @@
+document
+  .getElementById("queryForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    const data = {
+      department: document.getElementById("department").value,
+      description: document.getElementById("description").value,
+      queryType: document.getElementById("queryType").value,
+      contactNo: document.getElementById("contactNo").value,
+      priority: document.querySelector('input[name="priority"]:checked').value,
+    };
+
+    fetch("http://localhost:3000/submit-query", {
+      // Pointing to the correct URL
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        alert("Query submitted successfully");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
